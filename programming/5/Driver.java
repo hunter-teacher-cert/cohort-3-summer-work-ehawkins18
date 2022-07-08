edf843da57aef0a5a4fc4c41119c9f4b61cc8af6
Ed Hawkins <96295230+ehawkins18@users.noreply.github.com>
2022-07-08 12:29:14 +0000

    Updated Time.java and Driver.java

--- programming/5/Driver.java
+++ programming/5/Driver.java
@@ -3,9 +3,9 @@ import java.util.*;
 
 
 /**
- * Time class by Team LucidThinkeren
- * First Last
- * collaborators: First Last, First Last
+ * Time class by Team Timex
+ * Ed Hawkins
+ * collaborators: Thea, Elizabeth, Chris
  */
 
 /**
@@ -17,16 +17,40 @@ import java.util.*;
    Place this file in a folder named programming/5/Driver.java
 */
 
-
 public class Driver {
 
-    public static void main(String[] args) {
+  public static void main(String[] args) {
 
-	//declare a var of appropriate type to assign an instance of Time to
-	Time t;
+  	//declare a var of appropriate type to assign an instance of Time to
+  	Time t1;
+  
+  	//assign var the address of a newly-apportioned Time object
+  	t1 = new Time();
 
-	//assign var the address of a newly-apportioned Time object
-	t = new Time();
-    }//end main()
+    //declare & assign a specific time
+    Time t2 = new Time(14, 45, 30);
+
+    //print out t1 and t2 
+    System.out.println("Time t1 is: " + t1);
+    System.out.println("Time t2 is: " + t2);
+
+    //Check if t1 and t2 are the same
+    System.out.println("Are t1 & t2 the same? " +  t1.equals(t2));
+
+    //Add some time to t1
+    t1.add(new Time(2, 30, 45));
+    System.out.println("After adding 2 hr, 30 min, and 45 sec, t1 is: " + t1);
+
+    //Compare t1 and t2
+    System.out.println("The difference between t1 and t2 is " +  t1.compareTo(t2)+" sec.");
+
+    //Add some time to t1
+    t1.add(new Time(10,50,8));
+    System.out.println("After adding 10 hours, 50 min, and 8 sec, t1 is: " + t1);
+
+    //Check if t1 and t2 are equal
+    System.out.println("Does t1 equal t2? " +  t1.equals(t2));
+     
+  }//end main()
 
 }//end class
--- programming/5/Time.java
+++ programming/5/Time.java
@@ -4,16 +4,16 @@ import java.util.*;
 
 /**
  * Time class by Team LucidThinkeren
- * First Last
- * collaborators: First Last, First Last
+ * Ed Hawkins
+ * collaborators: Thea, Elizabeth, Chris
  */
-
 /**
    INSTRUCTIONS:
    This file contains the starter code for our Time class.
    The class here uses ints for hours, minutes, and seconds but you
    could decide to change the internal representation to just store
    an int representing a number of seconds.
+      <We chose not to do this.>
    Place this file in a folder named programming/5/Time.java
    Basic level (complete all):
    - public Time(int hrs, int mins, int secs) - constructor
@@ -29,48 +29,41 @@ import java.util.*;
 */
 
 
-
 public class Time {
     // Instance Variable(s)
-    // You can change this if you want to use the alternate
-    // implementation of just storing the overall seconds.
-    
     int hours;
     int minutes;
     int seconds;
 
-    // Constructors
+    // Constructor #1: Default Constructor has no parameters
     public Time(){
-	this.hours = 0;
-	this.minutes = 0;
-	this.seconds = 0;
-	
+    	this.hours = 0;
+    	this.minutes = 0;
+    	this.seconds = 0;
     }
 
     /**
        Parameters:
        - hrs, mins, secs - the time you want to create the class as
        Initialize this instance to represent hrs:mins:secs as the time.
-       
      */
+    //Constructor #2; takes in 3 different parameters
     public Time(int hrs, int mins, int secs){
 
-	// your code here
+    	this.hours = hrs;
+    	this.minutes = mins;
+    	this.seconds = secs;
 	
     }
     
-    
-    // Methods
+    //Additional Methods
 
     /**
        returns a string representation of the time
     */
     public String toString(){
-	return("This should be changed to return the time in a nice form");
+	    return(hours + ":" + minutes + ":" + seconds);
     }
-
-
-    
 
     /**
        Parameters:
@@ -80,10 +73,10 @@ public class Time {
     public void set(int hrs, int mins, int secs){
 	// add the code to add the time represented by other
 	// to this instance.
-
+      this.hours = hrs;
+    	this.minutes = mins;
+    	this.seconds = secs;
     }
-
-    
 
     /**
        Parameters:
@@ -95,8 +88,32 @@ public class Time {
 	// add the code to add the time represented by other
 	// to this instance.
 
+      //convert both Time objects -- other and this current one -- to seconds
+      int otherSeconds = other.convertToSeconds();
+      int currentSeconds = convertToSeconds();
+
+      //add up the total seconds
+      int totalSeconds = otherSeconds + currentSeconds;
+
+      //determine the amount of hours, minutes and remaning seconds totalled
+      hours = totalSeconds / 3600;
+      totalSeconds %= 3600;
+
+      minutes = totalSeconds / 60;
+      totalSeconds %= 60;
+
+      seconds = totalSeconds;      
+
     }
 
+    public int convertToSeconds(){
+     
+      return (hours * 3600) + (minutes * 60) + (seconds);
+      
+    }
+
+
+  
 
     /**
        Parameters:
@@ -106,9 +123,10 @@ public class Time {
        false otherwise.
     */
     public boolean equals(Time other){
-	// your code here)
-
-	return false; // change this
+      if(hours == other.hours && minutes == other.minutes && seconds == other.seconds){
+        return true;
+      }
+      return false;      
     }
 
     /**
@@ -122,9 +140,28 @@ public class Time {
        0 if the two instances represent the same time.
     */
     public int compareTo(Time other){
-	// your code here)
+      
+      //convert both Time objects -- other and this current one -- to seconds
+      int otherSeconds = other.convertToSeconds();
+      int currentSeconds = this.convertToSeconds();
 
-	return 0; // change this
+      return currentSeconds - otherSeconds;
+      /*
+      //check if this is > other
+      if(currentSeconds > otherSeconds){
+        return 1;        
+      }
+      //check if other is > this
+      else if(otherSeconds > currentSeconds){
+        return -1;        
+      }
+      
+      //0 if the two instances represent the same time.
+      else{
+        return 0;
+      }
+      */
+
     }
     
 
